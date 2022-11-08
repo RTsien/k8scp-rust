fdf86dfa2f0c013b114d1c2ea500da1e8c4ba40f
RTsien <rtsien@qq.com>
2022-11-08 22:17:41 +0800

    support pod shell

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,66 @@
-use std::thread;
 use std::time::Duration;
 use std::{cmp::min, fmt::Write};
+use std::{fs, thread};
 
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
-use futures::{StreamExt, TryStreamExt};
 use k8s_openapi::api::core::v1::Pod;
-use tracing::*;
 use kube::{
-    api::{Api, AttachParams, DeleteParams, ListParams, PostParams, ResourceExt, WatchEvent},
-    Client,
+    api::{Api, AttachParams},
+    config::{KubeConfigOptions, Kubeconfig},
+    Client, Config,
 };
+use tracing::*;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[arg(short, long)]
-    name: String,
+    kubeconfig: String,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[arg(short, long, default_value = "default")]
+    namespace: String,
 
-    #[arg(short, long, default_value_t = 231231231)]
-    size: u64,
+    #[arg(short, long)]
+    pod: String,
+
+    #[arg(short, long, default_value = "")]
+    container: String,
+
+    #[arg(short, long)]
+    tty: bool,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {:?}!", args)
-    }
-    let mut downloaded = 0;
-    let total_size = args.size;
-
-    let pb = ProgressBar::new(total_size);
-    pb.set_style(ProgressStyle::with_template("{msg} {spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
-        .unwrap()
-        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
-        .progress_chars("#>-"));
-    pb.finish_with_message("downloaded");
-
-
-    while downloaded < total_size {
-        // let new = min(downloaded + 223211, total_size);
-        // downloaded = new;
-        // pb.set_position(new);
-        downloaded = min(downloaded + 223211, total_size);
-        pb.set_position(downloaded);
-        thread::sleep(Duration::from_millis(12));
-    }
+    let kubeconfig = fs::read_to_string(args.kubeconfig)?;
 
     tracing_subscriber::fmt::init();
-    let client = Client::try_default().await?;
+    // let client = Client::try_default().await?;
+    let client = Client::try_from(
+        Config::from_custom_kubeconfig(
+            Kubeconfig::from_yaml(kubeconfig.as_str())?,
+            &KubeConfigOptions {
+                context: None,
+                cluster: None,
+                user: None,
+            },
+        )
+        .await?,
+    )?;
 
-    let p: Pod = serde_json::from_value(serde_json::json!({
-        "apiVersion": "v1",
-        "kind": "Pod",
-        "metadata": { "name": "example" },
-        "spec": {
-            "containers": [{
-                "name": "example",
-                "image": "alpine",
-                // Do nothing
-                "command": ["tail", "-f", "/dev/null"],
-            }],
-        }
-    }))?;
-
-    let pods: Api<Pod> = Api::default_namespaced(client);
+    let pods: Api<Pod> = Api::namespaced(client, args.namespace.as_str());
     // Stop on error including a pod already exists or is still being deleted.
-    pods.create(&PostParams::default(), &p).await?;
-
-    // Wait until the pod is running, otherwise we get 500 error.
-    let lp = ListParams::default().fields("metadata.name=example").timeout(10);
-    let mut stream = pods.watch(&lp, "0").await?.boxed();
-    while let Some(status) = stream.try_next().await? {
-        match status {
-            WatchEvent::Added(o) => {
-                info!("Added {}", o.name_any());
-            }
-            WatchEvent::Modified(o) => {
-                let s = o.status.as_ref().expect("status exists on pod");
-                if s.phase.clone().unwrap_or_default() == "Running" {
-                    info!("Ready to attach to {}", o.name_any());
-                    break;
-                }
-            }
-            _ => {}
-        }
-    }
+    // pods.create(&PostParams::default(), &p).await?;
 
     // Do an interactive exec to a blog pod with the `sh` command
-    let ap = AttachParams::interactive_tty();
-    let mut attached = pods.exec("example", vec!["sh"], &ap).await?;
+    let mut ap = AttachParams::interactive_tty().tty(args.tty);
+    if args.container.len() > 0 {
+        ap = ap.container(args.container);
+    }
+    let mut attached = pods.exec(args.pod.as_str(), vec!["bash"], &ap).await?;
 
     // The received streams from `AttachedProcess`
     let mut stdin_writer = attached.stdin().unwrap();
@@ -108,23 +72,19 @@ async fn main() -> anyhow::Result<()> {
     let mut stdout = tokio::io::stdout();
     // pipe current stdin to the stdin writer from ws
     tokio::spawn(async move {
-        tokio::io::copy(&mut stdin, &mut stdin_writer).await.unwrap();
+        tokio::io::copy(&mut stdin, &mut stdin_writer)
+            .await
+            .unwrap();
     });
     // pipe stdout from ws to current stdout
     tokio::spawn(async move {
-        tokio::io::copy(&mut stdout_reader, &mut stdout).await.unwrap();
+        tokio::io::copy(&mut stdout_reader, &mut stdout)
+            .await
+            .unwrap();
     });
     // When done, type `exit\n` to end it, so the pod is deleted.
     let status = attached.take_status().unwrap().await;
-    info!("{:?}", status);
-
-    // Delete it
-    info!("deleting");
-    pods.delete("example", &DeleteParams::default())
-        .await?
-        .map_left(|pdel| {
-            assert_eq!(pdel.name_any(), "example");
-        });
+    info!("{}", status.unwrap().status.unwrap());
 
     Ok(())
 }
